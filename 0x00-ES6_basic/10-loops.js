74842ad0a4cad08e376aa277818200f0ee8cd98e
Oloishorua <jacoboloishorua@gmail.com>
2024-07-23 00:28:42 +0300

    task 11 - ES6 for loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const myarray = [];
+  for (const item of array) {
+    myarray.push(appendString + item);
+  }
+  return myarray;
+}
